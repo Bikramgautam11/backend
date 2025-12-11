73c29fab34d376b32423e1d7e8c442be8faa28a0
Bikram gautam <bikramgautam7288@gmail.com>
2025-12-12 00:02:19 +0200

    change

--- task-1.cpp
+++ task-1.cpp
@@ -11,7 +11,7 @@ int main() {
 
     for (size_t i = 0; i < days.size(); i++) {
         cout << days[i] << " = " << temperature[i] << "°C - Weather is";
-
+// Classify the weather based on temperature
         if (temperature[i] < 10)
             cout << " COLD";
         else if (temperature[i] <= 20)
